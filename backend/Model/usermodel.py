e61977a05a9e8ef56a7e38eb5e468fbcb2ad9dcf
lonely-code-cube <poppyseed7112004@gmail.com>
2022-04-03 11:56:08 +0530

    Created Channel model inside usermodel

--- backend/Model/usermodel.py
+++ backend/Model/usermodel.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class Channel(BaseModel):
+    id: str = Field(alias='_id')
+    name: str
+    public: bool
+    joined_at: int
+    meta: Optional[dict]
 
 class User(BaseModel):
-    ID: int
+    id: Optional[str] = Field(alias='_id')
     username: str
-    password: str
+    hash: str
+    channels: List[Channel]
     meta: Optional[dict]
