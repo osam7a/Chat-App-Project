79ec4ebf9abbc6e67d4a805ff9777c44661b3ff9
lonely-code-cube <poppyseed7112004@gmail.com>
2022-04-03 11:57:38 +0530

    Created register_user and modified create_channel function

--- backend/database/database.py
+++ backend/database/database.py
@@ -7,7 +7,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from backend.Model.usermodel import User
 from backend.errors.errormodel import Error
-from backend.Model.channelmodel import Channel
+from backend.Model import usermodel
+from backend.Model import channelmodel
 from backend.Model.messagemodel import Message
 
 from config._secrets import _MONGO_URI
@@ -17,27 +18,45 @@ class Database:
     def __init__(self):
         self.db = AsyncIOMotorClient(_MONGO_URI).main
 
-    async def create_channel(self, name: str, hash: str) -> Union[Channel, Error]:
+    async def register_user(self, username: str, hash: str) -> Union[usermodel.User, Error]:
+        collection = self.db.users
+        created_at = int(time())
+        try:
+            user = usermodel.User(
+                username=username,
+                hash=hash,
+                channels=[],
+            )
+            x = await collection.insert_one(user.dict(exclude={"id"}))
+            user.id = str(x.inserted_id)
+            return user
+        except Exception as e:
+            raise e
+            pass
+
+    async def create_channel(
+        self, name: str, hash: str, owner: channelmodel.User
+    ) -> Union[channelmodel.Channel, Error]:
         collection = self.db.channels
         created_at = int(time())
         try:
-            channel = Channel(
-                created_at=created_at, name=name, hash=hash, messages=[], users=[]
+            channel = channelmodel.Channel(
+                created_at=created_at, name=name, hash=hash, messages=[], users=[owner]
             )
-            x = await collection.insert_one(channel.dict(exclude={'id'}))
+            x = await collection.insert_one(channel.dict(exclude={"id"}))
             channel.id = str(x.inserted_id)
             return channel
         except Exception as e:
             return Error(message="Could not create channel", code=1)
 
-    async def fetch_channel(self, id: str) -> Union[Channel, Error]:
+    async def fetch_channel(self, id: str) -> Union[channelmodel.Channel, Error]:
         id = ObjectId(id)
         collection = self.db.channels
         channel = await collection.find_one({"_id": id})
         if not channel:
             return Error(message="Channel does not exist", code=404)
         else:
-            return Channel(
+            return channelmodel.Channel(
                 _id=str(channel["_id"]),
                 created_at=channel["created_at"],
                 name=channel["name"],
@@ -83,31 +102,6 @@ class Database:
                 direct_messages=user["direct_messages"],
             )
 
-    async def register_user(self, username, password):
-        collection = self.db.users
-        user = await collection.find_one({"username": username})
-        if not user:
-            uID = None
-            await collection.insert_one(
-                User(
-                    ID=uID,
-                    username=username,
-                    password=password,
-                    created_at=datetime.utcnow(),
-                    direct_messages=[
-                        {
-                            "user": self.chatAppUser,
-                            "messages": [
-                                Message(
-                                    author=self.chatAppUser.dict(),
-                                    content="Welcome to chatapp!, if you need help, contact us on discord osam7a#1017 or midnightFirefly#9122",
-                                    created_at=datetime.utcnow(),
-                                ).dict()
-                            ],
-                        }
-                    ],
-                ).dict()
-            )
 
     async def update_user(self, userID):
         collection = self.db.users
